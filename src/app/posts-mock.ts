809a7d3b24b702b6bbaec1bffcc8e2c09c3112cf
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-05 17:56:05 +0500

    added mock file for posts

---
+++ src/app/posts-mock.ts
@@ -0,0 +1,14 @@
+import { Post } from './core/models/post.model';
+
+export const POSTS: Post[] = [
+  { id: 1, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 2, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 3, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 4, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 5, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 6, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 7, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 8, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 9, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' },
+  { id: 10, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, et quae deserunt iste quos praesentium modi totam saepe ducimus impedit! Impedit soluta dignissimos maxime possimus magnam saepe. Ratione, aut nisi?' }
+];
