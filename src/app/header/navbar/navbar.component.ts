ba8f02b4e0dcfbcc9cd1fb3f5bde2d0fe1a97818
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-07 00:12:55 +0500

    Added dark view toggle state to navbar

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  theme: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setTheme(theme: boolean) {
+    this.theme = theme;
+    if (theme) {
+      document.querySelector('body').style.background = '#333';
+      document.querySelector('body').style.color = 'white';
+      document.querySelector('body').style.transition = 'all 300ms linear';
+    }
+    else {
+      document.querySelector('body').style.background = 'white';
+      document.querySelector('body').style.color = 'black';
+    }
+  }
 }
