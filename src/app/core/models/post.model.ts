d8f4bfa1e229fa827143ab2a24e4fdba5d314429
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-07 00:12:24 +0500

    Modified Post model

--- src/app/core/models/post.model.ts
+++ src/app/core/models/post.model.ts
@@ -1,4 +1,13 @@
+import { Image } from './image.model';
+
 export interface Post {
   id: number;
+  author: string;
+  title: string;
+  slug: string;
+  readtime: number;
   content: string;
+  created_at: Date;
+  updated_at: Date;
+  image: Image;
 };
